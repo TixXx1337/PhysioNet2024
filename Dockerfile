13b3947a296a8769560d0f3307eba5cd8b7f93cc
TixXx1337 <64852188+TixXx1337@users.noreply.github.com>
2024-07-08 13:46:49 +0200

    CinC_cleaned

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,6 @@ COPY ./ /challenge
 WORKDIR /challenge
 
 ## Install your dependencies here using apt install, etc.
-RUN pip install --upgrade pip
-RUN pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
 ## Include the following line if you have a requirements.txt file.
 RUN pip install -r requirements.txt
--- team_code.py
+++ team_code.py
@@ -9,95 +9,13 @@
 #
 ################################################################################
 
+import joblib
+import numpy as np
+import os
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 import sys
 
-import numpy as np
-
-sys.path.append("Digitized-vs-Image-ECG-classification")
-import joblib
-import pandas as pd
-import model_zoo
-from pathlib import Path
-import torch
-from PIL import Image
-from torch.utils.data import Dataset
-from tqdm import tqdm
-import copy
-import glob
-import torch.nn as nn
-import torch.optim as optim
-from trainer import Ecg12LeadImageNetTrainerBinary
-import Dataloader
-
 from helper_code import *
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-
-class ECG_Multilead_Dataset(Dataset):
-    def __init__(self, path_to_dataset=None, transform=None):
-        super().__init__()
-        self.data = None
-        self.data_debug = []
-        self.data_info = []
-        self.transform = transform
-        if type(path_to_dataset) is list:
-            df_dataset_all = []
-            for path in path_to_dataset:
-                header = glob.glob(f"{path}/*.hea")
-                df_dataset = pd.DataFrame({"header": header})
-                df_dataset["basename"] = df_dataset["header"].apply(lambda x: os.path.basename(x))
-                df_dataset["basename"] = df_dataset["basename"].apply(lambda x: x[:-4])
-                df_dataset = df_dataset.apply(func=get_class, axis=1)
-                df_dataset_all.append(df_dataset)
-            df_dataset_all = pd.concat(df_dataset_all, ignore_index=True)
-        else:
-            header = glob.glob(f"{path_to_dataset}/*.hea")
-            df_dataset = pd.DataFrame({"header": header})
-            df_dataset["basename"] = df_dataset["header"].apply(lambda x: os.path.basename(x))
-            df_dataset["basename"] = df_dataset["basename"].apply(lambda x: x[:-4])
-            df_dataset = df_dataset.apply(func=get_class, axis=1)
-            df_dataset_all = df_dataset
-        self.data = df_dataset_all
-        self.classes = {"Normal":0, "Abnormal":1}
-
-    def __len__(self):
-        return len(self.data)
-    def __getitem__(self, idx):
-        image = Image.open(self.data.iloc[idx]["image"]).convert('L')
-        image = self.load_image(image)
-        dx = self.data.iloc[idx]["Dx"]
-        dx = self.classes[dx.strip()]
-        return image, dx
-
-    def load_image(self, image):
-        """
-        Simple Functions that loads the image and reshapes them
-        :param image: PIL object
-        :return: image resized
-        """
-        image = image.resize((512, 512))
-        image = np.array(image)
-        image = image.reshape(512, 512, 1)
-        image=image/255
-        return image
-
-
-def get_class(row) -> str:
-    path = Path(row["header"])
-    #row["image"] = os.path.join(path.parent, f'{row["basename"]}-0.png')
-    with open(row["header"], "r") as f:
-        for line in f.readlines():
-            if "#" in line:
-                line = line.replace("#", "")
-                line = line.strip()
-                label, definition = line.split(":")
-                row[label.strip()] = definition.strip()
-    row["Image"] = row["Image"].split(",")[0].strip()  #Only use one image for training TODO: change for later implememtation
-    row["image"] = os.path.join(path.parent, row["Image"])
-    return row
-
 
 ################################################################################
 #
@@ -105,25 +23,35 @@ def get_class(row) -> str:
 #
 ################################################################################
 
+# Train your models. This function is *required*. You should edit this function to add your code, but do *not* change the arguments
+# of this function. If you do not train one of the models, then you can return None for the model.
+
 # Train your digitization model.
-def train_digitization_model(data_folder, model_folder, verbose):
-    # Find data files.
+def train_models(data_folder, model_folder, verbose):
+    # Find the data files.
     if verbose:
-        print('Training the digitization model...')
         print('Finding the Challenge data...')
 
     records = find_records(data_folder)
     num_records = len(records)
 
     if num_records == 0:
-        raise FileNotFoundError('No data was provided.')
+        raise FileNotFoundError('No data were provided.')
 
-    # Extract the features and labels.
+    # Train the digitization model. If you are not training a digitization model, then you can remove this part of the code.
+
+    if verbose:
+        print('Training the digitization model...')
+
+    # Extract the features and labels from the data.
     if verbose:
         print('Extracting features and labels from the data...')
 
-    features = list()
+    digitization_features = list()
+    classification_features = list()
+    classification_labels = list()
 
+    # Iterate over the records.
     for i in range(num_records):
         if verbose:
             width = len(str(num_records))
@@ -131,100 +59,73 @@ def train_digitization_model(data_folder, model_folder, verbose):
 
         record = os.path.join(data_folder, records[i])
 
-        # Extract the features from the image...
-        current_features = extract_features(record)
-        features.append(current_features)
+        # Extract the features from the image; this simple example uses the same features for the digitization and classification
+        # tasks.
+        features = extract_features(record)
+        
+        digitization_features.append(features)
 
-    # Train the model.
+        # Some images may not be labeled...
+        labels = load_labels(record)
+        if any(label for label in labels):
+            classification_features.append(features)
+            classification_labels.append(labels)
+
+    # ... but we expect some images to be labeled for classification.
+    if not classification_labels:
+        raise Exception('There are no labels for the data.')
+
+    # Train the models.
     if verbose:
-        print('Training the model on the data...')
+        print('Training the models on the data...')
 
-    # This overly simple model uses the mean of these overly simple features as a seed for a random number generator.
-    model = np.mean(features)
+    # Train the digitization model. This very simple model uses the mean of these very simple features as a seed for a random number
+    # generator.
+    digitization_model = np.mean(features)
 
-    # Create a folder for the model if it does not already exist.
+    # Train the classification model. If you are not training a classification model, then you can remove this part of the code.
+    
+    # This very simple model trains a random forest model with these very simple features.
+    classification_features = np.vstack(classification_features)
+    classes = sorted(set.union(*map(set, classification_labels)))
+    classification_labels = compute_one_hot_encoding(classification_labels, classes)
+
+    # Define parameters for random forest classifier and regressor.
+    n_estimators   = 12  # Number of trees in the forest.
+    max_leaf_nodes = 34  # Maximum number of leaf nodes in each tree.
+    random_state   = 56  # Random state; set for reproducibility.
+
+    # Fit the model.
+    classification_model = RandomForestClassifier(
+        n_estimators=n_estimators, max_leaf_nodes=max_leaf_nodes, random_state=random_state).fit(classification_features, classification_labels)
+
+    # Create a folder for the models if it does not already exist.
     os.makedirs(model_folder, exist_ok=True)
 
-    # Save the model.
-    save_digitization_model(model_folder, model)
+    # Save the models.
+    save_models(model_folder, digitization_model, classification_model, classes)
 
     if verbose:
         print('Done.')
         print()
 
+# Load your trained models. This function is *required*. You should edit this function to add your code, but do *not* change the
+# arguments of this function. If you do not train one of the models, then you can return None for the model.
+def load_models(model_folder, verbose):
+    digitization_filename = os.path.join(model_folder, 'digitization_model.sav')
+    digitization_model = joblib.load(digitization_filename)
 
-
-# Train your dx classification model.
-def train_dx_model(data_folder, model_folder, verbose):
-    hidden_channels = [8, 16, 32, 64, 128, 256, 512]
-    kernel_sizes = [5] * 7
-    dropout = 0.2
-    stride = 2
-    dilation = 1
-    batch_norm = True
-    fc_hidden_dims = [128]
-    num_of_classes = 2
-
-    model = model_zoo.Ecg12ImageNet(1, hidden_channels, kernel_sizes, 512, 512,
-                                 fc_hidden_dims, dropout=dropout, stride=stride,
-                                 dilation=dilation, batch_norm=batch_norm, num_of_classes=num_of_classes).to(device)
-
-
-
-    dataset = Dataloader.ECG_Multilead_Dataset(path_to_dataset=data_folder, verbose=True)
-    train_dl = torch.utils.data.DataLoader(dataset, batch_size=100, shuffle=True, num_workers=4)
-
-    lr = 0.01
-    epochs = 100
-    loss_fn = nn.BCEWithLogitsLoss()
-
-    optimizer = optim.Adam(model.parameters(), lr=lr)
-    trainer = Ecg12LeadImageNetTrainerBinary(model, loss_fn, optimizer, device)
-    fitResult = trainer.fit(train_dl,None, num_epochs=40, early_stopping=100, print_every=1)
-    torch.save(copy.deepcopy(model.state_dict()), os.path.join(model_folder,"dx_model.pt"))
-
-
-# Load your trained digitization model. This function is *required*. You should edit this function to add your code, but do *not*
-# change the arguments of this function. If you do not train a digitization model, then you can return None.
-def load_digitization_model(model_folder, verbose):
-    filename = os.path.join(model_folder, 'digitization_model.sav')
-    return joblib.load(filename)
-
-# Load your trained dx classification model. This function is *required*. You should edit this function to add your code, but do
-# *not* change the arguments of this function. If you do not train a dx classification model, then you can return None.
-def load_dx_model(model_folder, verbose):
-    #device = 0
-    hidden_channels = [8, 16, 32, 64, 128, 256, 512]
-    kernel_sizes = [5] * 7
-    dropout = 0.2
-    stride = 2
-    dilation = 1
-    batch_norm = True
-    fc_hidden_dims = [128]
-    num_of_classes = 2
-
-    model = model_zoo.Ecg12ImageNet(1, hidden_channels, kernel_sizes, 512, 512,
-                                 fc_hidden_dims, dropout=dropout, stride=stride,
-                                 dilation=dilation, batch_norm=batch_norm, num_of_classes=num_of_classes).to(device)
-
-    path = Path().resolve()
-
-    model_state_dict = torch.load(os.path.join(path, model_folder, 'dx_model.pt'))
-
-    model.load_state_dict(model_state_dict) #not required can be changed in FUTURE!!!
-
-    model.eval()
-
-    return model
-
+    classification_filename = os.path.join(model_folder, 'classification_model.sav')
+    classification_model = joblib.load(classification_filename)
+    return digitization_model, classification_model
 
 # Run your trained digitization model. This function is *required*. You should edit this function to add your code, but do *not*
-# change the arguments of this function.
-def run_digitization_model(digitization_model, record, verbose):
-    model = digitization_model['model']
+# change the arguments of this function. If you did not train one of the models, then you can return None for the model.
+def run_models(record, digitization_model, classification_model, verbose):
+    # Run the digitization model; if you did not train this model, then you can set signal = None.
 
-    # Extract features.
-    features = extract_features(record)
+    # Load the digitization model.
+    model = digitization_model['model']
 
     # Load the dimensions of the signal.
     header_file = get_header_file(record)
@@ -233,58 +134,29 @@ def run_digitization_model(digitization_model, record, verbose):
     num_samples = get_num_samples(header)
     num_signals = get_num_signals(header)
 
-    # For a overly simply minimal working example, generate "random" waveforms.
+    # Extract the features.
+    features = extract_features(record)
+    features = features.reshape(1, -1)
+
+    # Generate "random" waveforms using the a random seed from the features.
     seed = int(round(model + np.mean(features)))
-    signal = np.random.default_rng(seed=seed).uniform(low=-1000, high=1000, size=(num_samples, num_signals))
-    signal = np.asarray(signal, dtype=np.int16)
+    signal = np.random.default_rng(seed=seed).uniform(low=-1, high=1, size=(num_samples, num_signals))
+    
+    # Run the classification model; if you did not train this model, then you can set labels = None.
 
-    return signal
+    # Load the classification model and classes.
+    model = classification_model['model']
+    classes = classification_model['classes']
 
-# Run your trained dx classification model. This function is *required*. You should edit this function to add your code, but do
-# *not* change the arguments of this function.
-def run_dx_model(dx_model, record, signal, verbose):
-    model = dx_model
-    #if type(record) is list:
-    #    path = os.path.split(record[0])[0]
+    # Get the model probabilities.
+    probabilities = model.predict_proba(features)
+    probabilities = np.asarray(probabilities, dtype=np.float32)[:, 0, 1]
 
-    path = os.path.split(record)[0]
+    # Choose the class or classes with the highest probability as the label or labels.
+    max_probability = np.nanmax(probabilities)
+    labels = [classes[i] for i, probability in enumerate(probabilities) if probability == max_probability]
 
-
-    image_files =get_image_files(record)
-
-    images = []
-
-    for image_path in image_files:
-        image = Image.open(os.path.join(path,image_path)).convert('L')
-        image = image.resize((512, 512))
-        image = np.array(image)
-        #plt.imshow(image)
-        #plt.show()
-        image = image.reshape(512, 512, 1)
-        image = image / 255
-        images.append(image)
-
-    images = torch.utils.data.DataLoader(images, batch_size=32, shuffle=False) #TODO:Fix current validataion implementation (works until 32 images)
-
-
-    for image in iter(images):
-        image = image.transpose(1, 2).transpose(1, 3)
-        image = image.to(device, dtype=torch.float)
-        label = model(image)
-        labels =label.flatten().detach().to("cpu")
-
-    #labels = int(torch.sum(labels)/len(labels))
-    labels = torch.where(label < 0, 0, 1).to("cpu").flatten()
-    labels = list(np.array(labels))
-
-
-    classes = {0: "Normal", 1: "Abnormal"}
-
-    labels = [classes[key] for key in labels if key in classes]
-
-    return labels
-
-
+    return signal, labels
 
 ################################################################################
 #
@@ -294,7 +166,7 @@ def run_dx_model(dx_model, record, signal, verbose):
 
 # Extract features.
 def extract_features(record):
-    images = load_image(record)
+    images = load_images(record)
     mean = 0.0
     std = 0.0
     for image in images:
@@ -303,41 +175,14 @@ def extract_features(record):
         std += np.std(image)
     return np.array([mean, std])
 
-# Save your trained digitization model.
-def save_digitization_model(model_folder, model):
-    d = {'model': model}
-    filename = os.path.join(model_folder, 'digitization_model.sav')
-    joblib.dump(d, filename, protocol=0)
+# Save your trained models.
+def save_models(model_folder, digitization_model=None, classification_model=None, classes=None):
+    if digitization_model is not None:
+        d = {'model': digitization_model}
+        filename = os.path.join(model_folder, 'digitization_model.sav')
+        joblib.dump(d, filename, protocol=0)
 
-# Save your trained dx classification model.
-def save_dx_model(model_folder, model, classes):
-    d = {'model': model, 'classes': classes}
-    filename = os.path.join(model_folder, 'dx_model.sav')
-    joblib.dump(d, filename, protocol=0)
-
-
-
-
-
-
-#if __name__ == '__main__':
-    #train_dx_model("C:\\Users\\Tizian Dege\\PycharmProjects\\DeTECRohr\\PhysioNet2024\\ptb-xl\\test", model_folder="model", verbose=False)
-    #input = "ptb-xl/records100/00000"
-    #train_dx_model(input, model_folder="model", verbose=False)
-    #classes = {0:"Normal", 1:"Abnormal"}
-    #records = find_records(input)
-    #model = load_dx_model("model", verbose=True)
-    #model.eval()
-    #results_df = {"label":[], "record":[]}
-    #for record in tqdm(records, "Classifying Images"):
-    #    label = run_dx_model(dx_model=model, record=os.path.join(input,record), signal="ptb-xl\\testset\\00001_lr.dat",verbose=True)
-    #    label = int(torch.where(label<0, 0, 1))
-    #    output = os.path.join("output", record)
-    #    with open(output+".hea", "w") as f:
-    #        f.write(f"#Image: {classes[label]}")
-    #    results_df["label"].append(label)
-    #    results_df["record"].append(record)
-    #results_df = pd.DataFrame(results_df)
-    #count = results_df["label"].value_counts().to_dict()
-    #print(f"There were {count[0]} Normal and {count[1]} Abnormal Signals detected!")
-    #labels = run_dx_model(dx_model=model, record="ptb-xl\\testset\\00001_lr", signal="ptb-xl\t\estset\\00001_lr.dat", verbose=True)
+    if classification_model is not None:
+        d = {'model': classification_model, 'classes': classes}
+        filename = os.path.join(model_folder, 'classification_model.sav')
+        joblib.dump(d, filename, protocol=0)
